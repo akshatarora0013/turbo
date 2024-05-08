249ea5ac00e7ab069bf3a1723e1d03c759db8e7a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-08 14:13:38 +0900

    find_turbopack_part_id_in_asserts && PartId

--- crates/turbopack-ecmascript/src/references/mod.rs
+++ crates/turbopack-ecmascript/src/references/mod.rs
@@ -49,7 +49,7 @@ use swc_core::{
     },
 };
 use tracing::Instrument;
-use turbo_tasks::{TryJoinIterExt, Upcast, Value, ValueToString, Vc};
+use turbo_tasks::{vdbg, TryJoinIterExt, Upcast, Value, ValueToString, Vc};
 use turbo_tasks_fs::FileSystemPath;
 use turbopack_core::{
     compile_time_info::{CompileTimeInfo, FreeVarReference},
@@ -414,6 +414,7 @@ pub(crate) async fn analyse_ecmascript_module_internal(
     let parsed = if let Some(part) = part {
         let parsed = parse(source, ty, transforms);
         let split_data = split(path, source, parsed);
+        vdbg!(split_data, part);
         part_of_module(split_data, part)
     } else {
         parse(source, ty, transforms)
