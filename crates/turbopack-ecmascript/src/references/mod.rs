538c0f315679b0bd0abe20e96af36456f555cbc0
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-05-09 10:26:20 +0900

    Lint

--- crates/turbopack-ecmascript/src/references/mod.rs
+++ crates/turbopack-ecmascript/src/references/mod.rs
@@ -49,7 +49,7 @@ use swc_core::{
     },
 };
 use tracing::Instrument;
-use turbo_tasks::{vdbg, TryJoinIterExt, Upcast, Value, ValueToString, Vc};
+use turbo_tasks::{TryJoinIterExt, Upcast, Value, ValueToString, Vc};
 use turbo_tasks_fs::FileSystemPath;
 use turbopack_core::{
     compile_time_info::{CompileTimeInfo, FreeVarReference},
